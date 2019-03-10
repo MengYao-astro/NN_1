71743bb6f18c06cfe01ebb270004b77b12f20f80
Michael Keim <michaelkeim@Michaels-MacBook-Pro.local>
2019-03-10 16:34:35 +0100

    new file:   __pycache__/data_reader.cpython-36.pyc 	modified:   main.py 	renamed:    tets_out.csv -> test_out.csv

--- main.py
+++ main.py
@@ -1,44 +1,57 @@
 # Program to Classify 16x16 Images of Digits
 # Michael Keim (michaelkeim2468@gmail.com)
 import numpy as np
-import data_reader as datain
+import csv
+from sklearn.metrics import confusion_matrix 
 
-# Specify training set
-train_in, train_out = "data/train_in.csv", "data/train_out.csv"
+# Specify training and test sets
+train_in, train_out, test_in, test_out = "train_in.csv", "train_out.csv", "test_in.csv", "test_out.csv"
 
 # Using a priori number of digits
-digits, imsize, points = 10, datain.row_size(train_in), datain.col_size(train_in)
+digits = 10
+
+# Find image size and number of points
+with open(train_in) as csvfile:
+	reader = csv.reader(csvfile)
+	imsize, points = len(next(reader)), 1+len(list(reader))
+with open(test_in) as csvfile:
+	reader = csv.reader(csvfile)
+	test_points = len(list(reader))
+
+# Create matricies for data
+data_in, data_out = np.zeros((points, imsize)), np.zeros(points, dtype=int)
+test_data_in, test_data_out = np.zeros((test_points, imsize)), np.zeros(test_points, dtype=int)
 center, number = np.zeros((digits, imsize)), np.zeros(digits)
 
-# To avoid possible memory issues, data not stored in memory
-# First loop over rows to determine center and number of points per digit
-# Note that zip returns an iterator as of Python 3
-for row0, row1 in zip(datain.row_generator(train_in), datain.row_generator(train_out)):
-
-	# Add up sum for calculating mean
-	for i in range(imsize):
-		center[int(row1[0]), i] += float(row0[i])
-
-	# Count Digits
-	number[int(row1[0])] += 1.0
-
-# Divide by N to find center
+# Read in training set while calculating center and digit totals
+with open(train_in) as csvfile:
+	reader = csv.reader(csvfile)
+	i = 0
+	for row in reader:
+		data_in[i] = row
+		i += 1
+with open(train_out) as csvfile:
+	reader = csv.reader(csvfile)
+	i = 0
+	for row in reader:
+		data_out[i] = row[0]
+		number[data_out[i]] += 1.0
+		center[data_out[i]] += data_in[i]
+		i += 1
 for i in range(digits):
 	center[i,:] /= number[i]
 
-# Second loop over rows to determine maximum radii
-rmaxval2, rtemp, rmax = np.zeros(digits), np.zeros((digits, imsize)), np.zeros((digits, imsize))
-for row0, row1 in zip(datain.row_generator(train_in), datain.row_generator(train_out)):
+# Find maximum radii
+rtemp2, rmax2 = np.zeros(imsize), np.zeros((digits, imsize))
+for i in range(points):
 
 	# Find distance^2 from center for this image
-	for i in range(imsize):
-		rtemp[int(row1[0]), i] = (float(row0[i])-center[int(row1[0]), i])**2.0
+	for j in range(imsize):
+		rtemp2[j] = (data_in[i, j]-center[data_out[i], j])**2.0
 
 	# If distance^2 is greater than that stored for a given digit, replace
-	if rtemp.sum() > rmaxval2[int(row1[0])]:
-		rmaxval2[int(row1[0])] = rtemp.sum()
-		for i in range(imsize):
-			rmax[int(row1[0]), i] = rtemp[int(row1[0]), i]
+	if rtemp2.sum() > rmax2[data_out[i]].sum():
+		rmax2[data_out[i]] = rtemp2
 
 # Calculate distances between cloud centers and find the closest digits
 distance = np.zeros((digits, digits))
@@ -54,15 +67,32 @@ for i in range(digits):
 
 # Now classify based on distances
 dtemp = np.zeros(digits)
-dclass = np.zeros(points, dtype=int)
-n = 0
-for row0 in datain.row_generator(train_in):
+class_id = np.zeros(points, dtype=int)
+for k in range(points):
 	for i in range(digits):
 		for j in range(imsize):
-			dtemp[i] += (float(row0[j])-center[i,j])**2.0
-	dclass[n] = np.argmin(dtemp)
+			dtemp[i] += (data_in[k,j]-center[i,j])**2.0
+	class_id[k] = np.argmin(dtemp)
 	dtemp *= 0.0
-	n += 1
+print(confusion_matrix(data_out, class_id))
 
+"""
+# Read in test set
+with open(test_in) as csvfile:
+	reader = csv.reader(csvfile)
+	for row in reader:
+		test_data_in[i] = row
+		i += 1
+with open(train_out) as csvfile:
+	reader = csv.reader(csvfile)
+	i = 0
+	for row in reader:
+		data_out[i] = row[0]
+		number[data_out[i]] += 1.0
+		center[data_out[i]] += data_in[i]
+		i += 1
+for i in range(digits):
+	center[i,:] /= number[i]
+	"""
 
 
